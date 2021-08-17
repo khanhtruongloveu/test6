69ba30e17fef7f534ffc7eee813b93916ff1108f
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2021-08-17 23:04:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu cậu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Cậu với tớ là gì vậy',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu thích tớ đó nha :> ',
+    btnYes: 'Hơn cả tình bạn😍😍😍',
+    btnNo: 'Bình thường hoiii👍👍👍',
+    question:'Nói cho tới lý do vì sao cậu thích tớ đii <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì cậu đẹp trai, hài hước hihi <33333333',
+    mess: 'Tớ biết mà 🥰. You are the apple of my eye 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://www.facebook.com/profile.php?id=100034444426000' 
+}
